dcec304c88ea5f86acb8be6d23ec0c174a3501b9
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-04-01 02:54:35 +0300

    feat: add navbar with tedx logo, fix minor errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,40 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type {Metadata} from 'next';
+import {Geist, Geist_Mono} from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin']
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin']
 });
 
 export const metadata: Metadata = {
-  title: "TEDxNTUA Quiz",
-  description:
-    "A quiz to gauge which of the 9 TEDxNTUA's organizing team fits you best.",
+  title: 'TEDxNTUA Quiz',
+  description: "A quiz to gauge which of the 8 TEDxNTUA's organizing team fits you best."
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <nav className="h-[65px] w-full bg-black/95 flex items-center px-4">
+          <a
+            className="w-[85%] mx-auto"
+            href="https://www.tedxntua.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="tedxntua-logo.png" alt="Logo" className="h-[40px] w-auto" />
+          </a>
+        </nav>
         {children}
       </body>
     </html>
